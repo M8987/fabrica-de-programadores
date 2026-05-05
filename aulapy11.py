1e1b55c8f738210404da18c8944c97a77370d7cc
M8987 <marcolinoloko1411@gmail.com>
2026-05-05 15:17:47 -0300

    Add files via upload

---
+++ aulapy11.py
@@ -0,0 +1,10 @@
+pessoa = {"nome": "vanessa"}
+ "idade": 32
+"cor_cabelo": "marrom"
+
+
+
+pessoa["cor_cabelo"] = "loirona"
+pessoa["idade"] = 31
+pessoa["nome"] = "mari"
+print(pessoa["cor_cabelo"])
